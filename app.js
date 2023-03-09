53ded732d70069af7896a2434e2ec9de79fc237b
Okechukwu_victor <66298905+covstar@users.noreply.github.com>
2023-03-09 14:44:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
